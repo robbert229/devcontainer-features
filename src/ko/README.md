44779056896a1c71117d1a492f7bf493cd222195
John Rowley <johnrowleyster@gmail.com>
2023-08-30 04:06:31 +0000

    feat: added ko feature

---
+++ src/ko/README.md
@@ -0,0 +1,21 @@
+
+# Ko (ko)
+
+This feature adds ko. Its a fork of the upstream https://github.com/devcontainers-contrib/features/tree/main/src/ko feature which at the time of writing is broken.
+
+## Example Usage
+
+```json
+"features": {
+    "ghcr.io/robbert229/devcontainer-features/ko:1": {
+        "version": "v0.14.1"
+    }
+}
+```
+
+## Options
+
+
+---
+
+_Note: This file was auto-generated from the [devcontainer-feature.json](https://github.com/devcontainers/feature-starter/blob/main/src/hello/devcontainer-feature.json).  Add additional notes to a `NOTES.md`._
---
+++ src/ko/install.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# MIT License
+#
+# Copyright (c) 2022 devcontainers-contrib
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+set -e
+
+source ./library_scripts.sh
+
+# nanolayer is a cli utility which keeps container layers as small as possible
+# source code: https://github.com/devcontainers-contrib/nanolayer
+# `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
+# and if missing - will download a temporary copy that automatically get deleted at the end 
+# of the script
+ensure_nanolayer nanolayer_location "v0.5"
+
+
+$nanolayer_location \
+    install \
+    devcontainer-feature \
+    "ghcr.io/devcontainers-contrib/features/gh-release:1.0.18" \
+    --option repo='ko-build/ko' --option binaryNames='ko' --option version="$VERSION"
+    
+
+
+echo 'Done!'
